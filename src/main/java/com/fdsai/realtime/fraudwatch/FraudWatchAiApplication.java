d2ea7d68e5abd3fee2926e7d07e04b5d71bd3c34
felixojiambo <felixojiamboe@gmail.com>
2025-05-22 22:00:04 +0300

    enablescheduling

--- src/main/java/com/fdsai/realtime/fraudwatch/FraudWatchAiApplication.java
+++ src/main/java/com/fdsai/realtime/fraudwatch/FraudWatchAiApplication.java
@@ -2,7 +2,9 @@ package com.fdsai.realtime.fraudwatch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class FraudWatchAiApplication {
 
