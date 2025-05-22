12d8347a2699d80dddbc11de999d814061dd09de
felixojiambo <felixojiamboe@gmail.com>
2025-05-22 19:54:17 +0300

    feat: initialize Spring Boot project for real-time AI fraud detection pipeline

---
+++ src/main/java/com/fdsai/realtime/fraudwatch/FraudWatchAiApplication.java
@@ -0,0 +1,13 @@
+package com.fdsai.realtime.fraudwatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FraudWatchAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FraudWatchAiApplication.class, args);
+	}
+
+}
